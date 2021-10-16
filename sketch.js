7d72eb957ac0d071b3cb0c5afa39d2d5c19ef155
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-10-16 15:29:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -86,15 +86,7 @@ function keyPressed() {
 
   if(keyCode === 32){
     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle+PI/2;
-
-    var arrow = new PlayerArrow(posX, posY, 100, 10);
-
-    arrow.trajectory = [];
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
+    
 
   }
 }
@@ -103,9 +95,7 @@ function keyReleased () {
 
   if(keyCode === 32){
     //call shoot() function for each arrow in an array playerArrows
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle+PI/2;
-      playerArrows[playerArrows.length - 1].shoot(angle);
+   
     }
   }
 
